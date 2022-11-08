3930b8212d34bde3806f93bb38aaacc7768a95f7
owenerr <richmond.o@northeastern.edu>
2022-11-07 19:08:38 -0500

    some minor updates to game and card class info

--- Card.py
+++ Card.py
@@ -5,6 +5,15 @@ class Card:
     def __init__(self, value, suit) -> None:
         self.value = value
         self.suit = suit
+
+
+         
+
+        # Image to use for the sprite when face up
+        self.image_file_name = f":resources:images/cards/card{self.suit}{self.value}.png"
+
+        # Call the parent
+        # super().__init__(self.image_file_name, scale, hit_box_algorithm="None")
     
     def __str__(self) -> str:
         return f'{self.value} of {self.suit}'
--- game0.py
+++ game0.py
@@ -1,22 +1,35 @@
 import arcade
+from Suit import Suit
+from Card import Card
+from GameSlot import GameSlot
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
 
-arcade.open_window(SCREEN_WIDTH,SCREEN_HEIGHT, 'my first window')
 
-arcade.set_background_color(arcade.color.NAVAJO_WHITE)
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
 
-arcade.start_render()
 
-def draw_card(x_off, y_off,):
-    arcade.draw_rectangle_filled(x_off, y_off, 100, 175, arcade.color.WHITE)
-    
-    arcade.draw_arc_filled(150, 144, 85, 86,
-    arcade.color.BOTTLE_GREEN, 90, 230, 20, 100)
+# class Cardprop(arcade.sprite):
+#     pass
 
-    
-draw_card(150,200)
-arcade.finish_render()
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH,SCREEN_HEIGHT,'first attempt')
+        arcade.set_background_color(arcade.color.NAVAJO_WHITE)
+    def draw_card(x_off, y_off,):
+        arcade.draw_rectangle_filled(x_off, y_off, 100, 175, arcade.color.WHITE)
+        arcade.draw_rectangle_filled(x_off, y_off, 300, 475, arcade.color.WHITE)    
+        arcade.draw_arc_filled(150, 144, 85, 86,
+        arcade.color.BOTTLE_GREEN, 90, 230, 20, 100)
 
-arcade.run()
+
+
+def main():
+    window = Game()
+    # window.setup()
+    Game.draw_card(50,60)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
