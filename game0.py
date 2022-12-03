1b7b4be6dd3b4a484cf3d721bcaf9a6f5d66ff8b
AlexLaptop <alexangione419@gmail.com>
2022-12-03 16:03:31 -0500

    Fixed lag bug, startings of correct positions

--- game0.py
+++ game0.py
@@ -5,8 +5,9 @@ from GameSlot import GameSlot
 from Deck import Deck
 import math
 
-SCREEN_WIDTH = 1900
-SCREEN_HEIGHT = 1060
+
+SCREEN_WIDTH = 1898
+SCREEN_HEIGHT = 1074
 
 class Game(arcade.Window):
     def __init__(self):
@@ -15,6 +16,7 @@ class Game(arcade.Window):
         self.playableDeck = Deck(500, 500)
         self.playableDeck.shuffle()
         self.background = arcade.load_texture("background.png")
+
         for i in range(len(self.playableDeck.deck)):
             self.all_sprites.append(self.playableDeck.deck[i].sprite)
         self.cardzzz = []
@@ -25,8 +27,7 @@ class Game(arcade.Window):
         # arcade.set_background_color(arcade.color.BOTTLE_GREEN)
         
         arcade.start_render()
-        arcade.draw_texture_rectangle(650, 600, 1200,
-                                      1080, self.background)
+        arcade.draw_texture_rectangle(949, 537, 1898, 1074, self.background)
         dynamic_width_rect = math.floor(SCREEN_WIDTH/5)
         dynamic_height_rect = math.floor(SCREEN_HEIGHT/8)
         i = 0
@@ -41,11 +42,14 @@ class Game(arcade.Window):
         #     arcade.draw_rectangle_outline(SCREEN_WIDTH-x,SCREEN_HEIGHT-3*dynamic_height_rect,175,225,arcade.color.AIR_SUPERIORITY_BLUE,3)
         #     playslots[i] = GameSlot(SCREEN_WIDTH-x,SCREEN_HEIGHT-dynamic_height_rect)
         #     i+=1
+
         self.draw_deck()
     
    
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         self.cardzzz = arcade.get_sprites_at_point((x, y), self.all_sprites)
+        print(x,y)
+
     
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
         if len(self.cardzzz):
@@ -61,10 +65,19 @@ class Game(arcade.Window):
                     
 
     def draw_deck(self):
-        for i in range(len(self.playableDeck.deck)):
-            self.all_sprites.append(arcade.Sprite(self.playableDeck.deck[i].image,\
-             center_x = self.playableDeck.deck[i].centerx, center_y = self.playableDeck.deck[i].centery))
-        arcade.SpriteList.draw(self.all_sprites)
+        # deck = self.playableDeck.deck
+        # current_slot_x = 135
+        # cards_to_place = 1
+        # for i in range(0, 7):
+        #     current_slot_y = 615
+        #     for c in range(cards_to_place):
+        #         deck[0].x = current_slot_x
+        #         deck[0].y = current_slot_y
+        #         deck.remove(0)
+        #         current_slot_y += 50
+        #     cards_to_place += 1
+        
+        self.all_sprites.draw()
 
 
 def main():
